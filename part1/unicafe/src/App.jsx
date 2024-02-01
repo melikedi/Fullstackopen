5dfc83c1a43475e7defe7b76cd80be66a3605e9c
melike <melikedi@gmail.com>
2024-02-01 22:16:27 +0300

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,12 +10,36 @@ function Display({option,voteCount}){
   )
 }
 function App() {
+  const [total, setTotal] = useState(0)
+  const [avarage, setAvarage] = useState(0)
+  const [positive, setPositive] = useState('')
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const handleGoodClick = () => {console.log('good clicked'); setGood(good+1)}
-  const handleBadClick = () => {console.log('bad clicked'); setBad(bad+1)}
-  const handleNeutralClick = () => {console.log('neutral clicked'); setNeutral(neutral+1)}
+  const computeStatistics = (good,bad,neutral) => {
+    const updatedTotal = good+bad+neutral;
+    setTotal(updatedTotal);
+    setPositive(good * 100/updatedTotal + '%');
+    setAvarage((good - bad)/updatedTotal);
+  }
+  const handleGoodClick = () => {
+    console.log('good clicked'); 
+    const updatedGood = good+1; 
+    setGood(updatedGood); 
+    computeStatistics(updatedGood,bad,neutral);
+  }
+  const handleBadClick = () => {
+    console.log('bad clicked'); 
+    const updatedBad = bad+1; 
+    setBad(updatedBad);
+    computeStatistics(good,updatedBad,neutral);
+  }
+  const handleNeutralClick = () => {
+    console.log('neutral clicked'); 
+    const updatedNeutral = neutral+1;
+    setNeutral(updatedNeutral);
+    computeStatistics(good,bad,updatedNeutral);
+  }
   return (
 
     <div>
@@ -27,6 +51,9 @@ function App() {
       <Display option='good' voteCount={good}/>
       <Display option='neutral' voteCount={neutral}/>
       <Display option='bad' voteCount={bad}/>
+      <Display option='all' voteCount={total}/>
+      <Display option='avarage' voteCount={avarage}/>
+      <Display option='positive' voteCount={positive}/>
     </div>
 
   )
