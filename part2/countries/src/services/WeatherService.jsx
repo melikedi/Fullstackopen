67532b94b0e90e72bb433951bdbca621bbff7d1d
melike <melikedi@gmail.com>
2024-02-13 01:05:19 +0300

    part2 completed

--- part2/countries/src/services/WeatherService.jsx
+++ part2/countries/src/services/WeatherService.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 const baseUrl ='https://api.openweathermap.org/data/2.5/weather'
-const openWeatherApiKey = "3565c9c7b6d1497ed085faa1abfa698c"
-const openWeatherApiKey1 = import.meta.env.VITE_SOME_KEY
+const openWeatherApiKey = import.meta.env.VITE_OPENWEATHER_APIKEY
 const getResult = (latlng) => {
     return axios.get(`${baseUrl}?lat=${latlng[0]}&lon=${latlng[1]}&appid=${openWeatherApiKey}`).then(response=> {
         return response.data
